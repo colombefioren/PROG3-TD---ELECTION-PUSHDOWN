7ccf0cf3a7b701fd7ccbebb548dc584e9f9cfe76
colombefioren <colomberakotonjanahary@gmail.com>
2026-02-19 16:13:48 +0300

    feat: implement count all votes method

--- src/main/java/com/election/repository/DataRetriever.java
+++ src/main/java/com/election/repository/DataRetriever.java
@@ -2,11 +2,33 @@ package com.election.repository;
 
 import com.election.db.DBConnection;
 
-public class DataRetriever {
+import java.sql.*;
+
+public class DataRetriever implements VoteRepository {
     private final DBConnection dbConnection;
     public DataRetriever() {
         this.dbConnection = new DBConnection();
     }
 
 
+    @Override
+    public long countAllVotes() {
+        String sql = "select count(id) from vote";
+
+        Connection conn = null;
+        Statement ps = null;
+        ResultSet rs = null;
+
+        try{
+            conn = dbConnection.getDBConnection();
+            ps = conn.createStatement();
+            rs = ps.executeQuery(sql);
+            if(!rs.next()){
+                throw new IllegalArgumentException("No votes found");
+            }
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
